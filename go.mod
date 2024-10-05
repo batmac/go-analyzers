d62b2c1347dd70aa28afeb5c242e026e3e0db27f
Baptiste Canton <bat@sbz.fr>
2024-10-05 11:38:12 +0200

    as go 1.23

--- go.mod
+++ go.mod
@@ -1,10 +1,10 @@
 module go-analyzers
 
-go 1.19
+go 1.23.2
 
-require golang.org/x/tools v0.6.0
+require golang.org/x/tools v0.26.0
 
 require (
-	golang.org/x/mod v0.8.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/mod v0.21.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,95 +2,104 @@ package main
 
 import (
 	"golang.org/x/tools/go/analysis/multichecker"
-	"golang.org/x/tools/go/analysis/passes/asmdecl"
-	"golang.org/x/tools/go/analysis/passes/assign"
-	"golang.org/x/tools/go/analysis/passes/atomic"
-	"golang.org/x/tools/go/analysis/passes/atomicalign"
-	"golang.org/x/tools/go/analysis/passes/bools"
-	"golang.org/x/tools/go/analysis/passes/buildssa"
-	"golang.org/x/tools/go/analysis/passes/buildtag"
-	"golang.org/x/tools/go/analysis/passes/cgocall"
-	"golang.org/x/tools/go/analysis/passes/composite"
-	"golang.org/x/tools/go/analysis/passes/copylock"
-	"golang.org/x/tools/go/analysis/passes/ctrlflow"
-	"golang.org/x/tools/go/analysis/passes/deepequalerrors"
-	"golang.org/x/tools/go/analysis/passes/directive"
-	"golang.org/x/tools/go/analysis/passes/errorsas"
-	"golang.org/x/tools/go/analysis/passes/fieldalignment"
-	"golang.org/x/tools/go/analysis/passes/findcall"
-	"golang.org/x/tools/go/analysis/passes/framepointer"
-	"golang.org/x/tools/go/analysis/passes/httpresponse"
-	"golang.org/x/tools/go/analysis/passes/ifaceassert"
-	"golang.org/x/tools/go/analysis/passes/inspect"
-	"golang.org/x/tools/go/analysis/passes/loopclosure"
-	"golang.org/x/tools/go/analysis/passes/lostcancel"
-	"golang.org/x/tools/go/analysis/passes/nilfunc"
-	"golang.org/x/tools/go/analysis/passes/nilness"
-	"golang.org/x/tools/go/analysis/passes/pkgfact"
-	"golang.org/x/tools/go/analysis/passes/printf"
-	"golang.org/x/tools/go/analysis/passes/reflectvaluecompare"
-	"golang.org/x/tools/go/analysis/passes/shadow"
-	"golang.org/x/tools/go/analysis/passes/shift"
-	"golang.org/x/tools/go/analysis/passes/sigchanyzer"
-	"golang.org/x/tools/go/analysis/passes/sortslice"
-	"golang.org/x/tools/go/analysis/passes/stdmethods"
-	"golang.org/x/tools/go/analysis/passes/stringintconv"
-	"golang.org/x/tools/go/analysis/passes/structtag"
-	"golang.org/x/tools/go/analysis/passes/testinggoroutine"
-	"golang.org/x/tools/go/analysis/passes/tests"
-	"golang.org/x/tools/go/analysis/passes/timeformat"
-	"golang.org/x/tools/go/analysis/passes/unmarshal"
-	"golang.org/x/tools/go/analysis/passes/unreachable"
-	"golang.org/x/tools/go/analysis/passes/unsafeptr"
-	"golang.org/x/tools/go/analysis/passes/unusedresult"
-	"golang.org/x/tools/go/analysis/passes/unusedwrite"
-	"golang.org/x/tools/go/analysis/passes/usesgenerics"
+	"golang.org/x/tools/go/analysis/passes/appends"             // Package appends defines an Analyzer that detects if there is only one variable in append.
+	"golang.org/x/tools/go/analysis/passes/asmdecl"             // Package asmdecl defines an Analyzer that reports mismatches between assembly files and Go declarations.
+	"golang.org/x/tools/go/analysis/passes/assign"              // Package assign defines an Analyzer that detects useless assignments.
+	"golang.org/x/tools/go/analysis/passes/atomic"              // Package atomic defines an Analyzer that checks for common mistakes using the sync/atomic package.
+	"golang.org/x/tools/go/analysis/passes/atomicalign"         // Package atomicalign defines an Analyzer that checks for non-64-bit-aligned arguments to sync/atomic functions.
+	"golang.org/x/tools/go/analysis/passes/bools"               // Package bools defines an Analyzer that detects common mistakes involving boolean operators.
+	"golang.org/x/tools/go/analysis/passes/buildssa"            // Package buildssa defines an Analyzer that constructs the SSA representation of an error-free package and returns the set of all functions within it.
+	"golang.org/x/tools/go/analysis/passes/buildtag"            // Package buildtag defines an Analyzer that checks build tags.
+	"golang.org/x/tools/go/analysis/passes/cgocall"             // Package cgocall defines an Analyzer that detects some violations of the cgo pointer passing rules.
+	"golang.org/x/tools/go/analysis/passes/composite"           // Package composite defines an Analyzer that checks for unkeyed composite literals.
+	"golang.org/x/tools/go/analysis/passes/copylock"            // Package copylock defines an Analyzer that checks for locks erroneously passed by value.
+	"golang.org/x/tools/go/analysis/passes/ctrlflow"            // Package ctrlflow is an analysis that provides a syntactic control-flow graph (CFG) for the body of a function.
+	"golang.org/x/tools/go/analysis/passes/deepequalerrors"     // Package deepequalerrors defines an Analyzer that checks for the use of reflect.DeepEqual with error values.
+	"golang.org/x/tools/go/analysis/passes/defers"              // Package defers defines an Analyzer that checks for common mistakes in defer statements.
+	"golang.org/x/tools/go/analysis/passes/directive"           // Package directive defines an Analyzer that checks known Go toolchain directives.
+	"golang.org/x/tools/go/analysis/passes/errorsas"            // The errorsas package defines an Analyzer that checks that the second argument to errors.As is a pointer to a type implementing error.
+	"golang.org/x/tools/go/analysis/passes/fieldalignment"      // Package fieldalignment defines an Analyzer that detects structs that would use less memory if their fields were sorted.
+	"golang.org/x/tools/go/analysis/passes/findcall"            // Package findcall defines an Analyzer that serves as a trivial example and test of the Analysis API.
+	"golang.org/x/tools/go/analysis/passes/framepointer"        // Package framepointer defines an Analyzer that reports assembly code that clobbers the frame pointer before saving it.
+	"golang.org/x/tools/go/analysis/passes/httpmux"             //
+	"golang.org/x/tools/go/analysis/passes/httpresponse"        // Package httpresponse defines an Analyzer that checks for mistakes using HTTP responses.
+	"golang.org/x/tools/go/analysis/passes/ifaceassert"         // Package ifaceassert defines an Analyzer that flags impossible interface-interface type assertions.
+	"golang.org/x/tools/go/analysis/passes/inspect"             // Package inspect defines an Analyzer that provides an AST inspector (golang.org/x/tools/go/ast/inspector.Inspector) for the syntax trees of a package.
+	"golang.org/x/tools/go/analysis/passes/loopclosure"         // Package loopclosure defines an Analyzer that checks for references to enclosing loop variables from within nested functions.
+	"golang.org/x/tools/go/analysis/passes/lostcancel"          // Package lostcancel defines an Analyzer that checks for failure to call a context cancellation function.
+	"golang.org/x/tools/go/analysis/passes/nilfunc"             // Package nilfunc defines an Analyzer that checks for useless comparisons against nil.
+	"golang.org/x/tools/go/analysis/passes/nilness"             // Package nilness inspects the control-flow graph of an SSA function and reports errors such as nil pointer dereferences and degenerate nil pointer comparisons.
+	"golang.org/x/tools/go/analysis/passes/pkgfact"             // The pkgfact package is a demonstration and test of the package fact mechanism.
+	"golang.org/x/tools/go/analysis/passes/printf"              // Package printf defines an Analyzer that checks consistency of Printf format strings and arguments.
+	"golang.org/x/tools/go/analysis/passes/reflectvaluecompare" // Package reflectvaluecompare defines an Analyzer that checks for accidentally using == or reflect.DeepEqual to compare reflect.Value values.
+	"golang.org/x/tools/go/analysis/passes/shadow"              // Package shadow defines an Analyzer that checks for shadowed variables.
+	"golang.org/x/tools/go/analysis/passes/shift"               // Package shift defines an Analyzer that checks for shifts that exceed the width of an integer.
+	"golang.org/x/tools/go/analysis/passes/sigchanyzer"         // Package sigchanyzer defines an Analyzer that detects misuse of unbuffered signal as argument to signal.Notify.
+	"golang.org/x/tools/go/analysis/passes/slog"                // Package slog defines an Analyzer that checks for mismatched key-value pairs in log/slog calls.
+	"golang.org/x/tools/go/analysis/passes/sortslice"           // Package sortslice defines an Analyzer that checks for calls to sort.Slice that do not use a slice type as first argument.
+	"golang.org/x/tools/go/analysis/passes/stdmethods"          // Package stdmethods defines an Analyzer that checks for misspellings in the signatures of methods similar to well-known interfaces.
+	"golang.org/x/tools/go/analysis/passes/stdversion"          // Package stdversion reports uses of standard library symbols that are "too new" for the Go version in force in the referring file.
+	"golang.org/x/tools/go/analysis/passes/stringintconv"       // Package stringintconv defines an Analyzer that flags type conversions from integers to strings.
+	"golang.org/x/tools/go/analysis/passes/structtag"           // Package structtag defines an Analyzer that checks struct field tags are well formed.
+	"golang.org/x/tools/go/analysis/passes/testinggoroutine"    // Package testinggoroutine defines an Analyzerfor detecting calls to Fatal from a test goroutine.
+	"golang.org/x/tools/go/analysis/passes/tests"               // Package tests defines an Analyzer that checks for common mistaken usages of tests and examples.
+	"golang.org/x/tools/go/analysis/passes/timeformat"          // Package timeformat defines an Analyzer that checks for the use of time.Format or time.Parse calls with a bad format.
+	"golang.org/x/tools/go/analysis/passes/unmarshal"           // The unmarshal package defines an Analyzer that checks for passing non-pointer or non-interface types to unmarshal and decode functions.
+	"golang.org/x/tools/go/analysis/passes/unreachable"         // Package unreachable defines an Analyzer that checks for unreachable code.
+	"golang.org/x/tools/go/analysis/passes/unsafeptr"           // Package unsafeptr defines an Analyzer that checks for invalid conversions of uintptr to unsafe.Pointer.
+	"golang.org/x/tools/go/analysis/passes/unusedresult"        // Package unusedresult defines an analyzer that checks for unused results of calls to certain pure functions.
+	"golang.org/x/tools/go/analysis/passes/unusedwrite"         // Package unusedwrite checks for unused writes to the elements of a struct or array object.
+	"golang.org/x/tools/go/analysis/passes/usesgenerics"        // Package usesgenerics defines an Analyzer that checks for usage of generic features added in Go 1.18.
 )
 
 func main() {
 	multichecker.Main(
-
-		asmdecl.Analyzer, // checks that assembly functions are declared correctly
-		assign.Analyzer,  // checks for useless assignments
-		atomic.Analyzer,  // checks for common mistaken usages of the sync/atomic package
+		appends.Analyzer,
+		asmdecl.Analyzer,
+		assign.Analyzer,
+		atomic.Analyzer,
 		atomicalign.Analyzer,
-		bools.Analyzer,     // checks for boolean expressions that can be simplified
-		buildssa.Analyzer,  // builds the SSA representation of a package
-		buildtag.Analyzer,  // checks that +build tags are valid and correctly positioned
-		cgocall.Analyzer,   // checks that cgo calls are valid
-		composite.Analyzer, // checks that composite literals are valid
-		copylock.Analyzer,  // checks for locks inadvertently passed by value
-		ctrlflow.Analyzer,  // checks for unreachable code
+		bools.Analyzer,
+		buildssa.Analyzer,
+		buildtag.Analyzer,
+		cgocall.Analyzer,
+		composite.Analyzer,
+		copylock.Analyzer,
+		ctrlflow.Analyzer,
 		deepequalerrors.Analyzer,
+		defers.Analyzer,
 		directive.Analyzer,
-		errorsas.Analyzer, // checks that errors are not compared with == or != (use errors.Is or errors.As instead)
+		errorsas.Analyzer,
 		fieldalignment.Analyzer,
-		findcall.Analyzer,     // checks that calls to runtime.SetFinalizer are valid
-		framepointer.Analyzer, // checks that the framepointer buildmode is used when using cgo
-		httpresponse.Analyzer, // checks that http response bodies are closed
-		ifaceassert.Analyzer,  // checks that type assertions on interfaces are valid
-		inspect.Analyzer,      // runs an arbitrary Go analysis function
-		loopclosure.Analyzer,  // checks that loop closures do not capture loop variables
-		lostcancel.Analyzer,   // checks that context cancelation signals are propagated
-		nilfunc.Analyzer,      // checks that nil function values are not called
+		findcall.Analyzer,
+		framepointer.Analyzer,
+		httpmux.Analyzer,
+		httpresponse.Analyzer,
+		ifaceassert.Analyzer,
+		inspect.Analyzer,
+		loopclosure.Analyzer,
+		lostcancel.Analyzer,
+		nilfunc.Analyzer,
 		nilness.Analyzer,
-		pkgfact.Analyzer, // checks that package facts are valid
-		printf.Analyzer,  // checks that printf-like invocations are valid
+		pkgfact.Analyzer,
+		printf.Analyzer,
 		reflectvaluecompare.Analyzer,
 		shadow.Analyzer,
-		shift.Analyzer,       // checks that shifts are valid
-		sigchanyzer.Analyzer, // checks that signal handlers are valid
+		shift.Analyzer,
+		sigchanyzer.Analyzer,
+		slog.Analyzer,
 		sortslice.Analyzer,
-		stdmethods.Analyzer,    // checks that methods on the standard library types are valid
-		stringintconv.Analyzer, // checks that string conversions are valid
-		structtag.Analyzer,     // checks that struct tags are valid
+		stdmethods.Analyzer,
+		stdversion.Analyzer,
+		stringintconv.Analyzer,
+		structtag.Analyzer,
 		testinggoroutine.Analyzer,
-		tests.Analyzer, // checks that test files are valid
+		tests.Analyzer,
 		timeformat.Analyzer,
-		unmarshal.Analyzer,    // checks that unmarshal calls are valid
-		unreachable.Analyzer,  // checks that unreachable code is not executed
-		unsafeptr.Analyzer,    // checks that unsafe pointer conversions are valid
-		unusedresult.Analyzer, // checks that results of calls to certain functions are used
+		unmarshal.Analyzer,
+		unreachable.Analyzer,
+		unsafeptr.Analyzer,
+		unusedresult.Analyzer,
 		unusedwrite.Analyzer,
 		usesgenerics.Analyzer,
 	)
